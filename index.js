0f7c90703bbe870a7904641a3675f2201bd1f7f5
paf <paf@titelfrei.de>
2020-11-24 16:21:40 +0100

    Rename variables to have clearer names

--- index.js
+++ index.js
@@ -11,34 +11,38 @@ exports.recordMessages = webdriver =>
         window.addEventListener("message", e => {
             window.__messageRecorder__.all.push(e.data);
             window.__messageRecorder__.last[e.data.type || 'unknown'] = e.data;
-            window.__messageRecorder__.last.all = e.data;
+            window.__messageRecorder__.last.any = e.data;
         });
     });
 
 
-exports.getLastMessage = (webdriver, type = null, wait = exports.messageRecorderSettings.defaultWaitingTime) =>
-    webdriver.executeScript((type, wait) => new Promise(resolve => {
-        const popLastMessage = () => {
-            const msg = window.__messageRecorder__.last[type || 'all'];
-            delete window.__messageRecorder__.last[type || 'all'];
-            resolve(msg);
-        }
-        if (window.__messageRecorder__.last[type || 'all']) {
-            popLastMessage();
-        } else {
-            setTimeout(() => {
-                if (window.__messageRecorder__.last[type || 'all']) {
-                    popLastMessage();
-                } else {
-                    resolve(null);
-                }
-            }, wait);
-        }
-    }), type, wait);
+exports.getLastMessage = (webdriver, type = null, waitingTime = exports.messageRecorderSettings.defaultWaitingTime) =>
+    webdriver.executeScript(
+        (type, waitingTime) => new Promise(resolve => {
+            const popLastMessage = () => {
+                const msg = window.__messageRecorder__.last[type || 'any'];
+                delete window.__messageRecorder__.last[type || 'any'];
+                resolve(msg);
+            }
+            if (window.__messageRecorder__.last[type || 'any']) {
+                popLastMessage();
+            } else {
+                setTimeout(() => {
+                    if (window.__messageRecorder__.last[type || 'any']) {
+                        popLastMessage();
+                    } else {
+                        resolve(null);
+                    }
+                }, waitingTime);
+            }
+        }),
+        type,
+        waitingTime
+    );
 
 
 exports.getAllMessages = (webdriver, type) =>
     webdriver.executeScript(
-        type => window.__messageRecorder__.all.filter(msg => !type || (msg.type === type))
-        , type
+        type => window.__messageRecorder__.all.filter(msg => !type || (msg.type === type)),
+        type
     );
